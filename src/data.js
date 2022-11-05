bd489fdf9d3543b0cebd8b0e5cd1a34ae1e4edb3
Cory Metcalf <cmetcalf8@gmail.com>
2022-11-04 19:58:21 -0600

    set up data.js model

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description: "Put the Description of project here",
+        image: "./project-1.gif",
+        link: "https://",
+    },
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description: "Put the Description of project here",
+        image: "./project-1.gif",
+        link: "https://",
+    },
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description: "Put the Description of project here",
+        image: "./project-1.gif",
+        link: "https://",
+    },
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description: "Put the Description of project here",
+        image: "./project-1.gif",
+        link: "https://",
+    },
+];
